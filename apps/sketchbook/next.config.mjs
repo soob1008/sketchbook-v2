f35f195b4b99770e536fbca8e4b488cbc45ca1d3
subin kim <90506668+soob1008@users.noreply.github.com>
2025-03-06 20:11:26 +0900

    [Config]: fix basePath

--- apps/sketchbook/next.config.mjs
+++ apps/sketchbook/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@workspace/ui'],
-  basePath: '/sketchbook',
+  basePath: '/sketchbook-v2',
   output: 'export',
+  assetPrefix: '/sketchbook-v2'
 };
 
 export default nextConfig;
