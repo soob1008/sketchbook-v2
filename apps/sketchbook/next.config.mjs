adc09123f2a85231e6026d5ac772d2121433ecdf
subin kim <1008sb354@gmail.com>
2025-03-07 16:59:38 +0900

    [Config]: update basePath

--- apps/sketchbook/next.config.mjs
+++ apps/sketchbook/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@workspace/ui'],
-  basePath: '/sketchbook-v2',
   // output: 'export',
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
