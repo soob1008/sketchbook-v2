e37af6acf962518c3a2cf64f6c6c1f5e72342d7a
subin kim <1008sb354@gmail.com>
2025-03-07 16:17:22 +0900

    [Config]: env test

--- apps/sketchbook/next.config.mjs
+++ apps/sketchbook/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   transpilePackages: ['@workspace/ui'],
   basePath: '/sketchbook-v2',
   // output: 'export',
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 export default nextConfig;
