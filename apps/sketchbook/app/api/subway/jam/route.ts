aa8d32a969f81b0a6ab1e19b82f094f2953e8834
subin kim <90506668+soob1008@users.noreply.github.com>
2025-03-25 20:47:34 +0900

    [feat]: add subway data/api, install react-query (#6)

---
+++ apps/sketchbook/app/api/subway/jam/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import subwayJamData from '@/public/subway.json';
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const line = searchParams.get('line');
+    const dateType = searchParams.get('dateType');
+    const station = searchParams.get('station');
+
+    const filtered = subwayJamData.find((item) => {
+      return (
+        String(item['호선']) === line &&
+        item['요일구분'] === dateType &&
+        `${item['출발역']} ${item['상하구분']}` === station
+      );
+    });
+
+    return NextResponse.json({
+      station: filtered,
+    });
+  } catch (error) {
+    console.error('Error fetching RSS feed:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch RSS feed' },
+      { status: 500 }
+    );
+  }
+}
