55c719c7376c95f6bbdb5b24ef5d87d0a825ea98
subin kim <1008sb354@gmail.com>
2025-09-24 13:21:57 +0900

    feat: 지뢰찾기 UI 커스텀, 셀 오픈 로직 추가

--- apps/sketchbook/features/game/minesweeper/MinesweeperContainer.tsx
+++ apps/sketchbook/features/game/minesweeper/MinesweeperContainer.tsx
@@ -8,6 +8,7 @@ type Cell = {
   col: number;
   open: boolean;
   isMine: boolean;
+  flagged: boolean;
   adjacentMines: number;
 };
 
@@ -30,6 +31,7 @@ const createBoard = (rows: number, cols: number, mines: number) => {
       col: colIndex,
       open: false,
       isMine: false,
+      flagged: false,
       adjacentMines: 0,
     }))
   );
@@ -70,18 +72,54 @@ const createBoard = (rows: number, cols: number, mines: number) => {
 
 export default function MinesweeperContainer() {
   const [board, setBoard] = useState(createBoard(9, 9, 10));
-
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [flag, setFlag] = useState(10);
   // 새 게임 시작
   const handleRestart = () => {
     setBoard(createBoard(9, 9, 10));
   };
 
+  // 깃 발 놓기
+
   // 셀 열기
-  const handleOpenCell = (cell: Cell) => {
-    console.log('open', cell);
+  const handleOpenCell = (e, cell: Cell) => {
+    if (isGameOver || cell.open) return;
+
+    // if (e.button === 2) {
+    // }
+
+    const newBoard = board.map(row => row.map(c => ({ ...c })));
+
+    const openCell = (r: number, c: number) => {
+      if (!newBoard[r] || !newBoard[r][c]) return;
+      const target = newBoard[r][c];
+      if (target.open || target?.isMine) return;
+
+      target.open = true;
+
+      if (target?.adjacentMines === 0) {
+        for (const [dr, dc] of DIRECTION) {
+          const nr = r + dr;
+          const nc = c + dc;
+          if (nr >= 0 && nc >= 0 && nr < newBoard.length && nc < newBoard[0].length) {
+            openCell(nr, nc);
+          }
+        }
+      }
+    };
+
+    if (cell.isMine) {
+      setIsGameOver(true);
+      newBoard[cell.row][cell.col].open = true;
+    } else {
+      openCell(cell.row, cell.col);
+    }
+
+    setBoard(newBoard);
   };
+
   return (
-    <div className="p-4 bg-red-100 w-[360px] rounded">
+    <div className="p-4 bg-gray-100 w-[360px] rounded">
       <h3 className="font-bold">지뢰찾기 초급</h3>
       <div className="flex items-center justify-center mt-6">
         <button type="button" onClick={handleRestart} className="p-1 bg-white rounded">
@@ -93,17 +131,22 @@ export default function MinesweeperContainer() {
         <span></span>
       </div>
       {/* 지뢰 찾기 보드 */}
-      <div className="flex flex-col gap-2 mt-4">
+      <div className="flex flex-col mt-4">
         {board.map((row, rowIndex) => {
           return (
-            <div key={`row-${rowIndex}`} className="flex items-center justify-around">
+            <div key={`row-${rowIndex}`} className="flex items-center">
               {row.map(cell => {
                 const { row, col, open, isMine, adjacentMines } = cell;
+
+                const openClass = open
+                  ? 'bg-gray-200 shadow-[inset_0.5px_0.5px_0_#666,inset_-0.5px_-0.5px_0_#fff]'
+                  : 'bg-gray-300 shadow-[0.5px_0.5px_0_#666,-0.5px_-0.5px_0_#fff]';
 
                 return (
                   <div
                     key={`cell-${row}-${col}`}
-                    className="flex items-center justify-center w-8 h-8 bg-gray-300 rounded-sm text-sm"
+                    className={`flex items-center justify-center w-9 h-9 text-sm font-bold 
+    border border-gray-400  ${openClass}`}
                   >
                     {open ? (
                       isMine ? (
@@ -112,7 +155,7 @@ export default function MinesweeperContainer() {
                         <span>{adjacentMines}</span>
                       )
                     ) : (
-                      <button onClick={() => handleOpenCell(cell)}>{cell.adjacentMines}</button>
+                      <button onClick={e => handleOpenCell(e, cell)} className="w-9 h-9"></button>
                     )}
                   </div>
                 );
